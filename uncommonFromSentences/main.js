30d3a0ce4b6f5d3e2f3976c4a58c47b59fb14846
Ahmed_Elzayat <113038956+Ahmed-Elzayat112@users.noreply.github.com>
2024-09-17 14:38:50 +0300

    feat: :sparkles: uncommonFromSentences solve

---
+++ uncommonFromSentences/main.js
@@ -0,0 +1,29 @@
+"use strict";
+function uncommonFromSentences(s1, s2) {
+    const mp = new Map();
+    let temp = "";
+    for (let i = 0; i < s1.length; i++) {
+        temp += s1[i];
+        if (i === s1.length - 1 || s1[i + 1] === " ") {
+            mp.set(temp, (mp.get(temp) || 0) + 1);
+            temp = "";
+            i++;
+        }
+    }
+    for (let i = 0; i < s2.length; i++) {
+        temp += s2[i];
+        if (i === s2.length - 1 || s2[i + 1] === " ") {
+            mp.set(temp, (mp.get(temp) || 0) + 1);
+            temp = "";
+            i++;
+        }
+    }
+    const result = [];
+    mp.forEach((value, key) => {
+        if (value === 1) {
+            result.push(key);
+        }
+    });
+    return result;
+}
+console.log(uncommonFromSentences("apple apple", "banana"));
